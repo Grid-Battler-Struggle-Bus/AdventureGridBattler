82495736ddb37ed9c1016e4565d2edad6b4c628d
WFost <winnf04@gmail.com>
2025-10-21 15:44:58 -0400

    Initial commit

---
+++ app/src/main/java/com/zybooks/test1/Characters.java
@@ -0,0 +1,11 @@
+package com.zybooks.test1;
+
+public class Characters {
+    int movement = 2;
+    int spriteId = R.drawable.char1_sprite;
+    int location[] = new int[2];
+    boolean deployed = false;
+    public int getSprite(){return spriteId;}
+
+}
+
